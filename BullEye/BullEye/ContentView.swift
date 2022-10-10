9956b32f1f1659e1070590f86aa55144a443f6b7
ductrunghachix <letsmile95@gmail.com>
2022-10-10 11:00:40 +0700

    Add text style

--- BullEye/BullEye/ContentView.swift
+++ BullEye/BullEye/ContentView.swift
@@ -11,17 +11,31 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("PUT THE BULLEYE AS CLOSE AS YOU CAN! 🎯")
-                .bold()
                 .kerning(2.0)
+                .bold()
                 .font(.footnote)
                 .multilineTextAlignment(.center)
-                .padding(20)
+                .padding(.horizontal, 15)
                 .lineSpacing(4.0)
             Text("89")
+                .kerning(-1)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            
             HStack{
                 Text("1")
+                    .kerning(-1)
+                    .bold()
+                    .font(.headline)
+                    .padding(.horizontal, 30)
                 Slider(value: .constant(50), in: 1.0...100.0)
                 Text("100")
+                    .kerning(-1)
+                    .bold()
+                    .font(.headline)
+                    .padding(.horizontal, 30)
+
+                
             }
             Button(action: {}){
                 Text("Hit Me")
@@ -34,7 +48,6 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
         ContentView()
-            .previewLayout(.fixed(width: 568, height: 320))
         
     }
 }
