768b0516af5da9d5d2b157813dd0661cf8acf695
ductrunghachix <letsmile95@gmail.com>
2022-10-28 08:51:37 +0700

    add darkmode

--- BullEye/BullEye/Views/ContentView.swift
+++ BullEye/BullEye/Views/ContentView.swift
@@ -14,46 +14,55 @@ struct ContentView: View {
     @State private var game: Game = .init()
 
     var body: some View {
-        VStack {
-            Text("PUT THE BULLEYE AS CLOSE AS YOU CAN! 🎯")
-                .kerning(2.0)
-                .bold()
-                .font(.footnote)
-                .multilineTextAlignment(.center)
-                .padding(.horizontal, 15)
-                .lineSpacing(4.0)
-            Text(String(game.target))
-                .kerning(-1)
-                .font(.largeTitle)
-                .fontWeight(.black)
-
-            HStack {
-                Text("1")
+        ZStack {
+//            rgb(229, 229, 229)
+            Color("BackgroundColor")
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("PUT THE BULLEYE AS CLOSE AS YOU CAN! 🎯")
+                    .kerning(2.0)
+                    .bold()
+                    .font(.footnote)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 15)
+                    .lineSpacing(4.0)
+                Text(String(game.target))
                     .kerning(-1)
-                    .bold()
-                    .font(.headline)
-                    .padding(.horizontal, 20)
-                Slider(value: $sliderValue, in: 1.0 ... 100.0)
-                Text("100")
-                    .kerning(-1)
-                    .bold()
-                    .font(.headline)
-                    .padding(.horizontal, 20)
-            }
-            
+                    .font(.largeTitle)
+                    .fontWeight(.black)
 
-            Button(action: {
-                print("Hello SwiftUI")
-                alertIsVisible = true
-            }) {
-                Text("Hit Me".uppercased())
-                    .bold()
-                    .font(.title3)
-            }
+                HStack {
+                    Text("1")
+                        .kerning(-1)
+                        .bold()
+                        .font(.headline)
+                        .padding(.horizontal, 20)
+                    Slider(value: $sliderValue, in: 1.0 ... 100.0)
+                    Text("100")
+                        .kerning(-1)
+                        .bold()
+                        .font(.headline)
+                        .padding(.horizontal, 20)
+                }
+
+                Button(action: {
+                    print("Hello SwiftUI")
+                    alertIsVisible = true
+                }) {
+                    Text("Hit Me".uppercased())
+                        .bold()
+                        .font(.title3)
+                }
                 .padding(20)
-                .background(Color.blue)
+                .background(
+                    ZStack{
+                        Color("ButtonColor")
+                        LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+                    }
+                )
                 .foregroundColor(Color.white)
                 .cornerRadius(21)
+//                .border(Color.white, width: 3
                 .alert("Hello there!", isPresented: $alertIsVisible) {
                     Button("Awesome!") {}
                 } message: {
@@ -62,11 +71,10 @@ struct ContentView: View {
                     Text("The slider's value is \(roundedValue).\n" +
                         "You scored \(self.game.points(sliderValue: roundedValue)) Points\n🎉🎉🎉")
                 }
+            }
         }
-        .background(Color.gray)
     }
-    
-} 
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
